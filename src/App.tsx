9a923eb75168e2a6a3ae2b5965fd52ebde5affcd
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-11-25 13:24:23 -0400

    add particles bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ParticlesBg from 'particles-bg';
 import './App.css';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo';
@@ -7,6 +8,9 @@ import Rank from './components/Rank';
 function App() {
   return (
     <div>
+      <div className="particles">
+        <ParticlesBg type="cobweb" color="#ADD8E6" bg={true} num={100} />
+      </div>
       <Navigation />
       <Logo />
       <Rank />
